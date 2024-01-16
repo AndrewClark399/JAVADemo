efddf48cbe36f720573fc659eea9fa49b15854ea
AndrewClark399 <andy396clark@hotmail.co.uk>
2024-01-16 10:49:02 +0000

    Including Modifiers Activity

--- src/people/App.java
+++ src/people/App.java
@@ -5,17 +5,28 @@ public class App {
 	public static void main(String[] args) {
 
 		PeopleCharacteristics ac = new PeopleCharacteristics();
-		ac.name = "Andrew";
-		ac.age = 30;
-		ac.city = "Hull";
-		ac.country = "United Kingdom";
+		ac.setName("Andrew");
+		ac.setAge(30);
+		ac.setCity("Hull");
+		ac.setCountry("United Kingdom");
 
 		PeopleCharacteristics pp = new PeopleCharacteristics();
-		pp.name = "Peter";
-		pp.age = 24;
-		pp.city = "New York";
-		pp.country = "United States";
+		pp.setName("Peter");
+		pp.setAge(24);
+		pp.setCity("New York");
+		pp.setCountry("United States");
 
+		PeopleCharacteristics[] people = { ac, pp };
+
+		// for each cat of cats
+		for (PeopleCharacteristics person : people) {
+			person.print();
+			person.getAge();
+		}
+		ac.setAge(ac.getAge() + 1);
+		ac.print();
+		pp.print();
+		ac.setAge(ac.getAge() + 1);
 		ac.print();
 		pp.print();
 	}
--- src/people/PeopleCharacteristics.java
+++ src/people/PeopleCharacteristics.java
@@ -2,17 +2,51 @@ package people;
 
 public class PeopleCharacteristics {
 
-	String name;
-	int age;
-	String city;
-	String country;
+	private String name;
+	private int age;
+	private String city;
+	private String country;
 
-	void print() {
-		System.out.println("Hello, my name is " + name + ",");
-		System.out.println("I am " + age + ",");
-		System.out.println("from " + city + ",");
-		System.out.println("in " + country);
+	public void print() {
+		System.out.println(
+				"Hello, my name is " + name + "," + "I am " + age + "," + "from " + city + "," + "in " + country);
 
+	}
+
+	public void setAge(int age) {
+		if (age >= 0 && age < 120) {
+			this.age = age;
+		} else {
+			System.out.println("Please provide an age between 0 and 120");
+		}
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
 	}
 
 }
